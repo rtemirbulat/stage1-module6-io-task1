dc8f1243053c3ff9db93053610c0f47fb437dd85
rtemirbulat <equalityisnotequality@gmail.com>
2024-02-26 18:08:38 +0600

    First commit

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,46 @@
 package com.epam.mjc.io;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 
 public class FileReader {
 
-    public Profile getDataFromFile(File file) {
-        return new Profile();
+    public Profile getDataFromFile(File file) throws IOException {
+        FileInputStream in = null;
+        String result = "";
+        try {
+            in = new FileInputStream(file.getPath());
+            int c;
+            while ((c = in.read()) != -1) {
+                result += (char) c;
+            }
+        } finally {
+            if (in != null) {
+                in.close();
+            }
+        }
+        System.out.println(result);
+        Map<String, String> keyValueMap = Arrays.stream(result.split("\n"))
+                .map(kv -> kv.split(": "))
+                .filter(kvArray -> kvArray.length == 2)
+                .collect(Collectors.toMap(kv -> kv[0], kv -> kv[1]));
+        keyValueMap.replaceAll((k,v)->v.replaceAll("\\p{Cntrl}", ""));
+        /* we use a newline as the delimiter to divide the input string into an array of key-value pairs.
+        Then, we further divide each pair using the equals symbol (=) by using the map procedure.
+        Finally, we remove any pairings that do not include exactly two elements
+        and compile the remaining pairs into a Map with associated keys and values.
+         */
+        Profile profileFromData = new Profile();
+        profileFromData.setAge(Integer.parseInt(keyValueMap.get("Age")));
+        profileFromData.setName(keyValueMap.get("Name"));
+        profileFromData.setEmail(keyValueMap.get("Email"));
+        profileFromData.setPhone(Long.parseLong(keyValueMap.get("Phone")));
+        return profileFromData;
     }
 }
